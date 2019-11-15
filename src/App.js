20ade065f2e3ce2955fe4bdf5741a66a1e7ea0c1
Oksana Melnik <oksana@dedact.nl>
2019-11-15 08:21:06 +0100

    display search and tiles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import icons from './icons'
+import Tile from './Tile/Tile.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      searching: false,
+      searchInput: ''
+  }
+
+  inputHandler = (e) => {
+      this.setState({searchInput: e.target.value});
+  }
+
+  render() {
+    const style = {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '1rem',
+        height: '3rem',
+
+    };
+
+      return (
+        <div className="App">
+        <form>
+            <input
+                type="text"
+                placeholder="Search 1,000,000 images"
+                value={this.state.searchInput}
+                onChange={this.inputHandler}
+                >
+            </input>
+        </form>
+
+        { this.state.searching ? null :
+            <div className="tile-list">
+                {icons.map(icon => {
+                  console.log(icon)
+                    return  (<Tile style={style} image={icon} key={icon.id}/>)
+                })
+                }
+            </div>
+        }
+
+
+
+        </div>
+      );
+    }
 }
 
 export default App;
