fe7334c0be24e6fa1378e63c975f872f1149a522
Oksana Melnik <oksana@dedact.nl>
2019-11-15 08:20:00 +0100

    add icons

---
+++ src/icons.js
@@ -0,0 +1,60 @@
+import create from './icons/create.svg'
+import camera from './icons/camera.svg'
+import canva from './icons/canva.svg'
+import chart from './icons/chart.svg'
+import filter from './icons/filter.svg'
+import grid from './icons/grid.svg'
+import image from './icons/image.svg'
+import line from './icons/line.svg'
+import weak from './icons/weak.svg'
+
+const icons = [
+  {
+    id: 1,
+    src: create,
+    text: 'create'
+  },
+  {
+    id: 2,
+    src: camera,
+    text: 'camera'
+  },
+  {
+    id: 3,
+    src: canva,
+    text: 'canva'
+  },
+  {
+    id: 4,
+    src: chart,
+    text: 'chart'
+  },
+  {
+    id: 5,
+    src: filter,
+    text: 'filter'
+  },
+  {
+    id: 6,
+    src: grid,
+    text: 'grid'
+  },
+  {
+    id: 7,
+    src: image,
+    text: 'image'
+  },
+  {
+    id: 8,
+    src: line,
+    text: 'line'
+  },
+  {
+    id: 9,
+    src: weak,
+    text: 'weak'
+  }
+
+]
+
+export default icons
